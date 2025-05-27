61f30351fbeb3869b6474e8caec60cd4c8c64c59
Angel Herrera <aghcx3@outlook.com>
2025-05-27 13:04:31 -0600

    Cuadrito

--- src/app/cuadrito/cuadrito.component.ts
+++ src/app/cuadrito/cuadrito.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-cuadrito',
-  imports: [],
+  imports: [CardModule],
   templateUrl: './cuadrito.component.html',
   styleUrl: './cuadrito.component.css'
 })
