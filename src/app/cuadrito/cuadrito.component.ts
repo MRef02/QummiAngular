1876d8846a64b13f9a763257acf35ad45c962ce9
Angel Herrera <aghcx3@outlook.com>
2025-05-27 14:12:05 -0600

    cuadrito esqueleto

--- src/app/cuadrito/cuadrito.component.ts
+++ src/app/cuadrito/cuadrito.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { BotonComponent } from "../boton/boton.component";
+import { FlechasComponent } from "../flechas/flechas.component";
+import { BotonHamburguesaComponent } from "../boton-hamburguesa/boton-hamburguesa.component";
 
 @Component({
   selector: 'app-cuadrito',
-  imports: [CardModule],
+  imports: [CardModule, BotonComponent, FlechasComponent, BotonHamburguesaComponent],
   templateUrl: './cuadrito.component.html',
   styleUrl: './cuadrito.component.css'
 })
