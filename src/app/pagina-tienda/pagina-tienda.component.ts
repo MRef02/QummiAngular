95d74ccbdeadc73a85448116e0cbe9f33ff8035f
Vainilla <larissitagarcia0703@gmail.com>
2025-06-02 19:19:50 -0600

    tienda2 y botones2

--- src/app/pagina-tienda/pagina-tienda.component.ts
+++ src/app/pagina-tienda/pagina-tienda.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pagina-tienda',
-  imports: [],
+  standalone: true, // 👈 Esto es CLAVE para que funcione correctamente
+  imports: [CommonModule], // 👈 Necesario para que funcione *ngIf, *ngFor, etc.
   templateUrl: './pagina-tienda.component.html',
-  styleUrl: './pagina-tienda.component.css'
+  styleUrls: ['./pagina-tienda.component.css'] // 👈 era "styleUrl", debe ser "styleUrls"
 })
 export class PaginaTiendaComponent {
+  style = {
+  width: '70vw',
+  height: '80vh',
+  overflow: 'hidden',
+  margin: 'auto'
+  };
+  pestanaActiva: 'coins' | 'adicionales' = 'coins';
 
+  cambiarPestana(pestana: 'coins' | 'adicionales') {
+    this.pestanaActiva = pestana;
+  }
+
+  volver() {
+    window.history.back();
+  }
 }
